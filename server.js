e2df20f875c550f2c6cf78cd9251d7fca8c755fb
Cameron <yeec327@gmail.com>
2021-03-27 00:16:58 -0700

    Built the bare bones of an express server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 // to use the express module, set it equal to a const variable. Usually, it is called "app"
 const app = express();
@@ -7,5 +7,3 @@ const app = express();
 // Use one if its methods called "listen". Tell it to listen on a specific port for any HTTP request that is sent to this server.
 
 app.listen(3000);
-
-
